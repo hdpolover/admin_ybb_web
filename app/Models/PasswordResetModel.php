74734f60b651698aa9c65daecbc2bf0220ee603c
masrudmubarok <masrud.mubarok21@gmail.com>
2025-03-23 23:58:29 +0700

    Add reset password req

---
+++ app/Models/PasswordResetModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PasswordResetModel extends Model
+{
+    protected $table = 'otp_requests';
+    protected $primaryKey = 'id';
+    protected $returnType = 'object';
+    // auto increment
+    protected $useAutoIncrement = true;
+
+    protected $allowedFields = ['email', 'otp', 'created_at'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    public function getOtp($email)
+    {
+        return $this->where('email', $email)->orderBy('created_at', 'DESC')->first();
+    }
+
+    public function getOtpByEmailAndOtp($email, $otp)
+    {
+        return $this->where('email', $email)->where('otp', $otp)->first();
+    }
+
+    public function createOtp($email, $otp)
+    {
+        return $this->save([
+            'email' => $email,
+            'otp' => $otp
+        ]);
+    }
+
+    public function deleteOtp($email)
+    {
+        return $this->where('email', $email)->delete();
+    }
+
+}
