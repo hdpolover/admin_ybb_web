6523b2f5a76cca73bca892573d7c5f3daa8df7f3
HendraMIT <suhendra@monarchintiteknologi.com>
2025-09-02 17:40:48 +0700

    Remove obsolete test files related to payment export functionality, including tests for export fixes, sorting, filtering, and API connections. These files are no longer needed as the related features have been updated or refactored.

--- test_export_curl.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST http://localhost:8080/exports/participants \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -H "X-Requested-With: XMLHttpRequest" \
-  -d "program_id=1&template=standard"
